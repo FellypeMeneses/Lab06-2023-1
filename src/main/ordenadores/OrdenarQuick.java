40d74ce1071834c1030204d3ce055bb66a27f0f1
FellypeMeneses <136759125+FellypeMeneses@users.noreply.github.com>
2023-12-14 20:03:15 +0000

    Commit Final

--- src/main/ordenadores/OrdenarQuick.java
+++ src/main/ordenadores/OrdenarQuick.java
@@ -6,9 +6,21 @@ public class OrdenarQuick implements Ordenador{
 
         return array;
     }
-
     public int quickParticio(int[] array, int inicio, int fim){
-        return 0;
+        int pivo = array[fim];
+        int i = inicio - 1;
+
+        for (int j = inicio; j < fim; j++) {
+            if (array[j] < pivo) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+              
     }
-    
+    return i;
+    }
 }
+
+
